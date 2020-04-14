7c618e3857eb1f74a44d5224e035a7fb34b3f5ba
Dariel Vasquez <darksalamence10@gmail.com>
2020-04-13 20:51:03 -0600

    PULL

--- frnproyectosw/src/componentes/Pages/Public/Home/Home.js
+++ frnproyectosw/src/componentes/Pages/Public/Home/Home.js
@@ -8,15 +8,12 @@ export default ({auth})=>{
         <Page pageURL="WELCOME?" auth={auth}>
             <section className="page-landing">
                 <div className="landing-photo col-s-12 col-m-9 col-10 no-padding">
-                    <h2 className="col-s-12">Bienvenidos al Futuro del Aprendizaje</h2>
-                    <Link className="button-3 col-s-8 col-m-5 col-3" to="/courses">Ver Cursos</Link>
+                    <h2 className="col-s-12">¡Aprender garífuna nunca ha sido más sencillo!</h2>                    
                 </div>
-                <h2 className="col-s-12">Populares</h2>
+                <h2 className="col-s-12">Cursos en línea</h2>
+                <Link className="button-3 col-s-8 col-m-5 col-3" to="/courses">Ver Cursos</Link>
                 <section className="courses col-s-12 col-m-3 col-2 no-padding">
                     <h2 className="col-s-12">Populares</h2>
-                    <div className="info"><span><DiHtml5/></span></div>
-                    <div className="info"><span><DiCss3/></span></div>
-                    <div className="info"><span><DiPhp/></span></div>
                 </section>
             </section>
         </Page>
